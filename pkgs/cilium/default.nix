74a032e7b137c95906bb89abe77a4e4bd6e0ceeb
luis <luis@pleme.io>
2025-03-26 15:57:51 -0300

    init

--- pkgs/cilium/default.nix
+++ pkgs/cilium/default.nix
@@ -1,102 +1,308 @@
 pkgs: {
-  cilium = let
-    rev = "v1.9.9"; # pinned release tag
+  # 1) cilium-bugtool
+  cilium-bugtool = let
     version = "1.9.9";
+    rev = "v${version}";
   in
     pkgs.buildGoModule rec {
-      pname = "cilium";
+      pname = "cilium-bugtool";
       inherit version;
 
+      # We'll reference cilium/cilium at v1.9.9
       src = pkgs.fetchFromGitHub {
         owner = "cilium";
         repo = "cilium";
         rev = rev;
-        # Force mismatch => On first build, Nix prints the correct SRI
-        sha256 = "sha256-OHgakSNqIbXYDC7cTw2fy0HlElQMilDbSD5SSjbYJhc=";
+        sha256 = pkgs.lib.fakeSha256; # mismatch => copy correct SRI
       };
 
-      # We'll also let Nix fail for vendored deps, so we can fix them
-      vendorHash = null;
+      vendorHash = pkgs.lib.fakeSha256; # also mismatch => fix after logs
 
-      # v1.9.9 sub-packages that exist:
-      # - daemon => agent
-      # - operator => cilium-operator
-      # - bugtool => cilium-bugtool
-      # - cilium-health => cilium-health
-      # - cilium-health/responder => cilium-health-responder
-      # - hubble-relay => hubble-relay
-      # - clustermesh-apiserver => clustermesh-apiserver
-      # - clustermesh-apiserver/debug => clustermesh-apiserver-debug
-      # - plugins/cilium-cni => cilium-cni
-      # - plugins/cilium-docker => cilium-docker
-      # (No cilium-dbg subdir in 1.9.9, so skip that.)
-      subPackages = [
-        "./daemon"
-        "./operator"
-        "./bugtool"
-        "./cilium-health"
-        "./cilium-health/responder"
-        "./hubble-relay"
-        "./clustermesh-apiserver"
-        "./clustermesh-apiserver/debug"
-        "./plugins/cilium-cni"
-        "./plugins/cilium-docker"
-      ];
+      # Just the bugtool subdir
+      subPackages = ["./bugtool"];
 
       doCheck = false;
 
-      nativeBuildInputs = [
-        pkgs.clang
-        pkgs.llvm
-      ];
+      # No clang/llvm needed for bugtool (no BPF)
+      nativeBuildInputs = [];
 
-      ldflagsArray = [
-        "-s"
-        "-w"
-        "-buildvcs=false"
-      ];
-
+      # We rename the built binary (default is "bugtool") to "cilium-bugtool"
       installPhase = ''
         runHook preInstall
         mkdir -p "$out/bin"
-
-        # main agent => compiled as "daemon"
-        [ -f "$GOPATH/bin/daemon" ] && cp "$GOPATH/bin/daemon" "$out/bin/cilium-agent"
-
-        # operator => "operator"
-        [ -f "$GOPATH/bin/operator" ] && cp "$GOPATH/bin/operator" "$out/bin/cilium-operator"
-
-        # bugtool => "bugtool"
         [ -f "$GOPATH/bin/bugtool" ] && cp "$GOPATH/bin/bugtool" "$out/bin/cilium-bugtool"
-
-        # cilium-health => "cilium-health"
-        [ -f "$GOPATH/bin/cilium-health" ] && cp "$GOPATH/bin/cilium-health" "$out/bin/"
-
-        # cilium-health-responder => "responder"
-        if [ -f "$GOPATH/bin/responder" ]; then
-          cp "$GOPATH/bin/responder" "$out/bin/cilium-health-responder"
-        fi
-
-        # hubble-relay => "hubble-relay"
-        [ -f "$GOPATH/bin/hubble-relay" ] && cp "$GOPATH/bin/hubble-relay" "$out/bin/"
-
-        # clustermesh-apiserver => "clustermesh-apiserver"
-        [ -f "$GOPATH/bin/clustermesh-apiserver" ] && cp "$GOPATH/bin/clustermesh-apiserver" "$out/bin/"
-
-        # clustermesh-apiserver/debug => "debug"
-        if [ -f "$GOPATH/bin/debug" ]; then
-          cp "$GOPATH/bin/debug" "$out/bin/clustermesh-apiserver-debug"
-        fi
-
-        # cilium-cni => "cilium-cni"
-        [ -f "$GOPATH/bin/cilium-cni" ] && cp "$GOPATH/bin/cilium-cni" "$out/bin/"
-
-        # cilium-docker => "cilium-docker"
-        [ -f "$GOPATH/bin/cilium-docker" ] && cp "$GOPATH/bin/cilium-docker" "$out/bin/"
-
         runHook postInstall
       '';
 
       name = "${pname}-${version}";
     };
+
+  # 2) cilium-docker
+  # cilium-docker = let
+  #   version = "1.9.9";
+  #   rev = "v${version}";
+  # in
+  #   pkgs.buildGoModule rec {
+  #     pname = "cilium-docker";
+  #     inherit version;
+  #
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "cilium";
+  #       repo = "cilium";
+  #       rev = rev;
+  #       sha256 = pkgs.lib.fakeSha256;
+  #     };
+  #
+  #     vendorHash = pkgs.lib.fakeSha256;
+  #
+  #     subPackages = ["./plugins/cilium-docker"];
+  #
+  #     doCheck = false;
+  #
+  #     # Also no BPF compile here
+  #     nativeBuildInputs = [];
+  #
+  #     installPhase = ''
+  #       runHook preInstall
+  #       mkdir -p "$out/bin"
+  #       [ -f "$GOPATH/bin/cilium-docker" ] && cp "$GOPATH/bin/cilium-docker" "$out/bin/cilium-docker"
+  #       runHook postInstall
+  #     '';
+  #
+  #     name = "${pname}-${version}";
+  #   };
+
+  # 3) cilium-cni
+  # cilium-cni = let
+  #   version = "1.9.9";
+  #   rev = "v${version}";
+  # in
+  #   pkgs.buildGoModule rec {
+  #     pname = "cilium-cni";
+  #     inherit version;
+  #
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "cilium";
+  #       repo = "cilium";
+  #       rev = rev;
+  #       sha256 = pkgs.lib.fakeSha256;
+  #     };
+  #
+  #     vendorHash = pkgs.lib.fakeSha256;
+  #
+  #     subPackages = ["./plugins/cilium-cni"];
+  #
+  #     doCheck = false;
+  #
+  #     # No BPF needed, just the CNI plugin
+  #     nativeBuildInputs = [];
+  #
+  #     installPhase = ''
+  #       runHook preInstall
+  #       mkdir -p "$out/bin"
+  #       [ -f "$GOPATH/bin/cilium-cni" ] && cp "$GOPATH/bin/cilium-cni" "$out/bin/cilium-cni"
+  #       runHook postInstall
+  #     '';
+  #
+  #     name = "${pname}-${version}";
+  #   };
+
+  # 4) cilium-health (main + responder)
+  # cilium-health = let
+  #   version = "1.9.9";
+  #   rev = "v${version}";
+  # in
+  #   pkgs.buildGoModule rec {
+  #     pname = "cilium-health";
+  #     inherit version;
+  #
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "cilium";
+  #       repo = "cilium";
+  #       rev = rev;
+  #       sha256 = pkgs.lib.fakeSha256;
+  #     };
+  #
+  #     vendorHash = pkgs.lib.fakeSha256;
+  #
+  #     # Two sub-packages: cilium-health and the responder
+  #     subPackages = [
+  #       "./cilium-health"
+  #       "./cilium-health/responder"
+  #     ];
+  #
+  #     doCheck = false;
+  #
+  #     nativeBuildInputs = [];
+  #
+  #     installPhase = ''
+  #       runHook preInstall
+  #       mkdir -p "$out/bin"
+  #       # cilium-health => "cilium-health"
+  #       [ -f "$GOPATH/bin/cilium-health" ] && cp "$GOPATH/bin/cilium-health" "$out/bin/"
+  #
+  #       # The responder => built as "responder"
+  #       if [ -f "$GOPATH/bin/responder" ]; then
+  #         cp "$GOPATH/bin/responder" "$out/bin/cilium-health-responder"
+  #       fi
+  #
+  #       runHook postInstall
+  #     '';
+  #
+  #     name = "${pname}-${version}";
+  #   };
+
+  # 5) cilium-operator
+  # cilium-operator = let
+  #   version = "1.9.9";
+  #   rev = "v${version}";
+  # in
+  #   pkgs.buildGoModule rec {
+  #     pname = "cilium-operator";
+  #     inherit version;
+  #
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "cilium";
+  #       repo = "cilium";
+  #       rev = rev;
+  #       sha256 = pkgs.lib.fakeSha256;
+  #     };
+  #
+  #     vendorHash = pkgs.lib.fakeSha256;
+  #
+  #     subPackages = ["./operator"];
+  #
+  #     doCheck = false;
+  #
+  #     nativeBuildInputs = [
+  #       # No BPF compile, but might want clang if operator does something special
+  #     ];
+  #
+  #     installPhase = ''
+  #       runHook preInstall
+  #       mkdir -p "$out/bin"
+  #       [ -f "$GOPATH/bin/operator" ] && cp "$GOPATH/bin/operator" "$out/bin/cilium-operator"
+  #       runHook postInstall
+  #     '';
+  #
+  #     name = "${pname}-${version}";
+  #   };
+  #
+  # # 6) cilium-agent (main daemon)
+  # cilium-agent = let
+  #   version = "1.9.9";
+  #   rev = "v${version}";
+  # in
+  #   pkgs.buildGoModule rec {
+  #     pname = "cilium-agent";
+  #     inherit version;
+  #
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "cilium";
+  #       repo = "cilium";
+  #       rev = rev;
+  #       sha256 = pkgs.lib.fakeSha256;
+  #     };
+  #
+  #     vendorHash = pkgs.lib.fakeSha256;
+  #
+  #     # The big code is in daemon/
+  #     subPackages = ["./daemon"];
+  #
+  #     doCheck = false;
+  #
+  #     # The agent might compile eBPF => clang/llvm recommended
+  #     nativeBuildInputs = [
+  #       pkgs.clang
+  #       pkgs.llvm
+  #     ];
+  #
+  #     installPhase = ''
+  #       runHook preInstall
+  #       mkdir -p "$out/bin"
+  #       # built as "daemon", rename to cilium-agent
+  #       [ -f "$GOPATH/bin/daemon" ] && cp "$GOPATH/bin/daemon" "$out/bin/cilium-agent"
+  #       runHook postInstall
+  #     '';
+  #
+  #     name = "${pname}-${version}";
+  #   };
+
+  # 7) hubble-relay
+  # hubble-relay = let
+  #   version = "1.9.9";
+  #   rev = "v${version}";
+  # in
+  #   pkgs.buildGoModule rec {
+  #     pname = "hubble-relay";
+  #     inherit version;
+  #
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "cilium";
+  #       repo = "cilium";
+  #       rev = rev;
+  #       sha256 = pkgs.lib.fakeSha256;
+  #     };
+  #
+  #     vendorHash = pkgs.lib.fakeSha256;
+  #
+  #     subPackages = ["./hubble-relay"];
+  #
+  #     doCheck = false;
+  #
+  #     nativeBuildInputs = [];
+  #
+  #     installPhase = ''
+  #       runHook preInstall
+  #       mkdir -p "$out/bin"
+  #       [ -f "$GOPATH/bin/hubble-relay" ] && cp "$GOPATH/bin/hubble-relay" "$out/bin/hubble-relay"
+  #       runHook postInstall
+  #     '';
+  #
+  #     name = "${pname}-${version}";
+  #   };
+
+  # 8) clustermesh-apiserver (and debug)
+  # clustermesh-apiserver = let
+  #   version = "1.9.9";
+  #   rev = "v${version}";
+  # in
+  #   pkgs.buildGoModule rec {
+  #     pname = "clustermesh-apiserver";
+  #     inherit version;
+  #
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "cilium";
+  #       repo = "cilium";
+  #       rev = rev;
+  #       sha256 = pkgs.lib.fakeSha256;
+  #     };
+  #
+  #     vendorHash = pkgs.lib.fakeSha256;
+  #
+  #     # We build both the main server & its debug subdir if present in 1.9.9
+  #     subPackages = [
+  #       "./clustermesh-apiserver"
+  #       "./clustermesh-apiserver/debug"
+  #     ];
+  #
+  #     doCheck = false;
+  #
+  #     nativeBuildInputs = [];
+  #
+  #     installPhase = ''
+  #       runHook preInstall
+  #       mkdir -p "$out/bin"
+  #       [ -f "$GOPATH/bin/clustermesh-apiserver" ] && cp "$GOPATH/bin/clustermesh-apiserver" "$out/bin/clustermesh-apiserver"
+  #
+  #       # The debug subpackage might produce "debug"
+  #       if [ -f "$GOPATH/bin/debug" ]; then
+  #         cp "$GOPATH/bin/debug" "$out/bin/clustermesh-apiserver-debug"
+  #       fi
+  #
+  #       runHook postInstall
+  #     '';
+  #
+  #     name = "${pname}-${version}";
+  #   };
 }
