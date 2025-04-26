3a48bd4c283558f6b13290f8f7251b2b48247c90
luis <luis@pleme.io>
2025-04-26 15:20:12 -0300

    init

--- hive.nix
+++ hive.nix
@@ -1,63 +1,57 @@
-{inputs, ...}: {
+{inputs, ...}:
+inputs.colmena.lib.makeHive {
   meta = {
+    # Pin Nixpkgs for consistent builds
     nixpkgs = import inputs.nixpkgs {system = "x86_64-linux";};
   };
 
-  defaults = {
-    config,
-    pkgs,
-    ...
-  }: {
-    # Shared things all nodes inherit (optional):
+  defaults = {pkgs, ...}: {
+    # Common configuration for all nodes
     environment.systemPackages = with pkgs; [vim wget curl];
     system.stateVersion = "24.05";
   };
 
-  nodes = {
-    "master" = {
-      deployment.targetHost = "192.168.1.10";
-      deployment.tags = ["masters"];
-
-      config = {
-        config,
-        pkgs,
-        ...
-      }: {
-        imports = [./modules];
-        blackmatter.components.kubernetes.enable = true;
-        networking.hostName = "master";
-        networking.firewall.allowedTCPPorts = [6443];
-      };
+  # Node definitions as top-level attributes (not nested under "nodes")
+  master = {
+    deployment.targetHost = "192.168.1.10";
+    deployment.tags = ["masters"];
+    config = {
+      config,
+      pkgs,
+      ...
+    }: {
+      imports = [./modules];
+      blackmatter.components.kubernetes.enable = true;
+      networking.hostName = "master";
+      networking.firewall.allowedTCPPorts = [6443];
     };
+  };
 
-    "worker-1" = {
-      deployment.targetHost = "192.168.1.11";
-      deployment.tags = ["workers"];
-
-      config = {
-        config,
-        pkgs,
-        ...
-      }: {
-        imports = [./modules];
-        blackmatter.components.kubernetes.enable = true;
-        networking.hostName = "worker-1";
-      };
+  "worker-1" = {
+    deployment.targetHost = "192.168.1.11";
+    deployment.tags = ["workers"];
+    config = {
+      config,
+      pkgs,
+      ...
+    }: {
+      imports = [./modules];
+      blackmatter.components.kubernetes.enable = true;
+      networking.hostName = "worker-1";
     };
+  };
 
-    "worker-2" = {
-      deployment.targetHost = "192.168.1.12";
-      deployment.tags = ["workers"];
-
-      config = {
-        config,
-        pkgs,
-        ...
-      }: {
-        imports = [./modules];
-        blackmatter.components.kubernetes.enable = true;
-        networking.hostName = "worker-2";
-      };
+  "worker-2" = {
+    deployment.targetHost = "192.168.1.12";
+    deployment.tags = ["workers"];
+    config = {
+      config,
+      pkgs,
+      ...
+    }: {
+      imports = [./modules];
+      blackmatter.components.kubernetes.enable = true;
+      networking.hostName = "worker-2";
     };
   };
 }
