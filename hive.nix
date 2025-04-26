5a66128da3c6fea8f256d3d7770bf8c197de051c
luis <luis@pleme.io>
2025-04-26 14:44:42 -0300

    init

--- hive.nix
+++ hive.nix
@@ -11,9 +11,47 @@ inputs.colmena.lib.makeHive {
       ...
     }: {
       imports = [./modules];
-      kubernetes.enable = true;
+      blackmatter.components.kubernetes.enable = true;
       networking.hostName = "master";
+      networking.firewall.allowedTCPPorts = [6443];
       system.stateVersion = "24.05";
+
+      deployment = {
+        targetHost = "192.168.1.10"; # <-- important
+        tags = ["masters"];
+      };
+    };
+
+    worker-1 = {
+      config,
+      pkgs,
+      ...
+    }: {
+      imports = [./modules];
+      blackmatter.components.kubernetes.enable = true;
+      networking.hostName = "worker-1";
+      system.stateVersion = "24.05";
+
+      deployment = {
+        targetHost = "192.168.1.11"; # <-- important
+        tags = ["workers"];
+      };
+    };
+
+    worker-2 = {
+      config,
+      pkgs,
+      ...
+    }: {
+      imports = [./modules];
+      blackmatter.components.kubernetes.enable = true;
+      networking.hostName = "worker-2";
+      system.stateVersion = "24.05";
+
+      deployment = {
+        targetHost = "192.168.1.12"; # <-- important
+        tags = ["workers"];
+      };
     };
   };
 }
