3897f0d4dd8582d5b650c4fac8698e5d284160eb
luis <luis@pleme.io>
2025-04-27 01:51:11 -0300

    init

--- infra/scale_and_probe.rb
+++ infra/scale_and_probe.rb
@@ -131,15 +131,16 @@ begin
   system('rm -rf dyanamic-nodes.nix')
   system('colmena build')
   system('ruby fetch_ips.rb')
-  # system('colmena apply')
+  sleep 20
+  system('colmena apply')
 ensure
-  # asg_names.each do |name|
-  #   log "Scaling #{name} back to 0"
-  #   begin
-  #     scale_asg(asg, name, 0)
-  #   rescue StandardError
-  #     log("Could not scale #{name}: #{$ERROR_INFO}")
-  #   end
-  # end
+  asg_names.each do |name|
+    log "Scaling #{name} back to 0"
+    begin
+      scale_asg(asg, name, 0)
+    rescue StandardError
+      log("Could not scale #{name}: #{$ERROR_INFO}")
+    end
+  end
   log 'Cleanup complete'
 end
