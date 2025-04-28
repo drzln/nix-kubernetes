7d0d4003bc6f6f680e7d412714346806f2613e64
luis <luis@pleme.io>
2025-04-28 20:08:08 -0300

    init

---
+++ modules/kubernetes/services/controller.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  config,
+  require,
+  ...
+}: let
+  cfg = config.kubernetes;
+  isCP = cfg.role == "master" || cfg.role == "single";
+  ctrlPkg = require "kube-controller-manager";
+in
+  lib.mkIf isCP {
+    systemd.services.kube-controller-manager = {
+      description = "Kubernetes controller-manager";
+      wantedBy = ["multi-user.target"];
+      after = ["kube-apiserver.service"];
+      serviceConfig = {
+        ExecStart =
+          "${ctrlPkg}/bin/kube-controller-manager "
+          + "--kubeconfig=/etc/kubernetes/controller-manager.conf "
+          + "--leader-elect=true";
+        Restart = "always";
+      };
+    };
+  }
