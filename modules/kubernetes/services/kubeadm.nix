8b73b90be0addbc8c9a18bd1cf656a48a1d88605
luis <luis@pleme.io>
2025-04-28 20:07:04 -0300

    init

---
+++ modules/kubernetes/services/kubeadm.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  config,
+  require,
+  ...
+}: let
+  cfg = config.kubernetes;
+  kubeadm = require "kubeadm";
+  isMaster = cfg.role == "master" || cfg.role == "single";
+  isWorker = cfg.role == "worker";
+in {
+  systemd.services = lib.mkMerge [
+    (lib.mkIf isMaster {
+      kubeadm-init = {
+        description = "kubeadm init (first boot)";
+        wantedBy = ["multi-user.target"];
+        after = ["containerd.service"];
+        before = ["kube-apiserver.service"];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${kubeadm}/bin/kubeadm init --skip-token-print --config /etc/kubeadm-init.yaml";
+          RemainAfterExit = true;
+        };
+      };
+    })
+
+    (lib.mkIf isWorker {
+      kubeadm-join = {
+        description = "kubeadm join (first boot)";
+        wantedBy = ["multi-user.target"];
+        after = ["containerd.service"];
+        before = ["kubelet.service"];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = lib.concatStringsSep " " [
+            "${kubeadm}/bin/kubeadm"
+            "join"
+            cfg.join.address
+            "--token"
+            cfg.join.token
+            "--discovery-token-ca-cert-hash"
+            cfg.join.caHash
+          ];
+          RemainAfterExit = true;
+        };
+      };
+    })
+  ];
+}
