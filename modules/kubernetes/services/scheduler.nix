c33b2a0d86a359443283d7bc24af655187e87ca1
luis <luis@pleme.io>
2025-04-28 20:08:21 -0300

    init

---
+++ modules/kubernetes/services/scheduler.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  config,
+  require,
+  ...
+}: let
+  cfg = config.kubernetes;
+  isCP = cfg.role == "master" || cfg.role == "single";
+  schedPkg = require "kube-scheduler";
+in
+  lib.mkIf isCP {
+    systemd.services.kube-scheduler = {
+      description = "Kubernetes scheduler";
+      wantedBy = ["multi-user.target"];
+      after = ["kube-apiserver.service"];
+      serviceConfig = {
+        ExecStart =
+          "${schedPkg}/bin/kube-scheduler "
+          + "--kubeconfig=/etc/kubernetes/scheduler.conf "
+          + "--leader-elect=true";
+        Restart = "always";
+      };
+    };
+  }
