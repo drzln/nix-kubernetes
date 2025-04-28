10e2c5926a2332cd38efa7cc3816bb2f82bb28ea
luis <luis@pleme.io>
2025-04-28 20:02:29 -0300

    init

---
+++ modules/kubernetes/assertions.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.kubernetes;
+  isWorker = cfg.role == "worker";
+in {
+  assertions = [
+    {
+      condition = elem cfg.role ["master" "worker" "single"];
+      message = "kubernetes.role must be master / worker / single.";
+    }
+    {
+      condition =
+        !isWorker
+        || (cfg.join.address != "" && cfg.join.token != "" && cfg.join.caHash != "");
+      message = "worker nodes need join.{address,token,caHash} set.";
+    }
+  ];
+}
