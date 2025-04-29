c2c0388b5bd217a6cadf79ea8c1e6062d6753396
drzzln <drzzln@protonmail.com>
2025-04-29 17:51:12 -0300

    init

--- modules/kubernetes/options.nix
+++ modules/kubernetes/options.nix
@@ -1,65 +1,66 @@
 {lib, ...}:
 with lib; {
   options.kubernetes = {
-    enable = mkEnableOption "bare-metal Kubernetes";
+    enable = mkEnableOption "Kubernetes";
+
     role = mkOption {
       type = types.enum ["master" "worker" "single"];
       default = "master";
     };
 
-    overlay = mkOption {
-      type = types.nullOr types.anything;
-      default = null;
-    };
+    # overlay = mkOption {
+    #   type = types.nullOr types.anything;
+    #   default = null;
+    # };
 
-    etcdPackage = mkOption {
-      type = types.nullOr types.package;
-      default = null;
-    };
+    # etcdPackage = mkOption {
+    #   type = types.nullOr types.package;
+    #   default = null;
+    # };
 
-    containerdPackage = mkOption {
-      type = types.nullOr types.package;
-      default = null;
-    };
+    # containerdPackage = mkOption {
+    #   type = types.nullOr types.package;
+    #   default = null;
+    # };
 
-    nodePortRange = mkOption {
-      type = types.nullOr types.str;
-      default = null;
-    };
+    # nodePortRange = mkOption {
+    #   type = types.nullOr types.str;
+    #   default = null;
+    # };
 
-    extraApiArgs = mkOption {
-      type = types.listOf types.str;
-      default = [];
-    };
+    # extraApiArgs = mkOption {
+    #   type = types.listOf types.str;
+    #   default = [];
+    # };
 
-    extraKubeletOpts = mkOption {
-      type = types.listOf types.str;
-      default = [];
-    };
+    # extraKubeletOpts = mkOption {
+    #   type = types.listOf types.str;
+    #   default = [];
+    # };
 
-    kubeadmExtra = mkOption {
-      type = types.str;
-      default = "";
-    };
+    # kubeadmExtra = mkOption {
+    #   type = types.str;
+    #   default = "";
+    # };
 
-    firewallOpen = mkOption {
-      type = types.bool;
-      default = false;
-    };
+    # firewallOpen = mkOption {
+    #   type = types.bool;
+    #   default = false;
+    # };
 
-    join.address = mkOption {
-      type = types.str;
-      default = "";
-    };
+    # join.address = mkOption {
+    #   type = types.str;
+    #   default = "";
+    # };
 
-    join.token = mkOption {
-      type = types.str;
-      default = "";
-    };
+    # join.token = mkOption {
+    #   type = types.str;
+    #   default = "";
+    # };
 
-    join.caHash = mkOption {
-      type = types.str;
-      default = "";
-    };
+    # join.caHash = mkOption {
+    #   type = types.str;
+    #   default = "";
+    # };
   };
 }
