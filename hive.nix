fba8832545f700ba71cca7bcf267850d5c239215
luis <luis@pleme.io>
2025-04-29 17:45:24 -0300

    init

--- hive.nix
+++ hive.nix
@@ -19,42 +19,42 @@ inputs.colmena.lib.makeHive {
     deployment.targetHost = "192.168.1.10";
   };
 
-  master-2 = {pkgs, ...}: {
-    imports = [./modules/kubernetes];
-    kubernetes = {
-      enable = true;
-      role = "master";
-      etcdPackage = pkgs.etcd;
-      containerdPackage = pkgs.containerd;
-    };
-    networking.hostName = "master-2";
-    deployment.targetHost = "192.168.1.20";
-  };
+  # master-2 = {pkgs, ...}: {
+  #   imports = [./modules/kubernetes];
+  #   kubernetes = {
+  #     enable = true;
+  #     role = "master";
+  #     etcdPackage = pkgs.etcd;
+  #     containerdPackage = pkgs.containerd;
+  #   };
+  #   networking.hostName = "master-2";
+  #   deployment.targetHost = "192.168.1.20";
+  # };
 
-  worker = {pkgs, ...}: {
-    imports = [./modules/kubernetes];
-    kubernetes = {
-      enable = true;
-      role = "worker";
-      etcdPackage = pkgs.etcd;
-      containerdPackage = pkgs.containerd;
-      join.address = "192.168.1.10:6443";
-      join.token = "abcdef.0123456789abcdef";
-      join.caHash = "sha256:deadbeefcafebabe0123456789abcdef0123456789abcdef0123456789abcd";
-    };
-    networking.hostName = "worker";
-    deployment.targetHost = "192.168.1.11";
-  };
+  # worker = {pkgs, ...}: {
+  #   imports = [./modules/kubernetes];
+  #   kubernetes = {
+  #     enable = true;
+  #     role = "worker";
+  #     etcdPackage = pkgs.etcd;
+  #     containerdPackage = pkgs.containerd;
+  #     join.address = "192.168.1.10:6443";
+  #     join.token = "abcdef.0123456789abcdef";
+  #     join.caHash = "sha256:deadbeefcafebabe0123456789abcdef0123456789abcdef0123456789abcd";
+  #   };
+  #   networking.hostName = "worker";
+  #   deployment.targetHost = "192.168.1.11";
+  # };
 
-  single = {pkgs, ...}: {
-    imports = [./modules/kubernetes];
-    kubernetes = {
-      enable = true;
-      role = "single";
-      etcdPackage = pkgs.etcd;
-      containerdPackage = pkgs.containerd;
-    };
-    networking.hostName = "single";
-    deployment.targetHost = "192.168.1.12";
-  };
+  # single = {pkgs, ...}: {
+  #   imports = [./modules/kubernetes];
+  #   kubernetes = {
+  #     enable = true;
+  #     role = "single";
+  #     etcdPackage = pkgs.etcd;
+  #     containerdPackage = pkgs.containerd;
+  #   };
+  #   networking.hostName = "single";
+  #   deployment.targetHost = "192.168.1.12";
+  # };
 }
