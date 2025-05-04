1e395b5da52b305c8d59088b296b4ec4ee6cf297
luis <luis@pleme.io>
2025-05-04 03:41:01 -0300

    init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,11 +9,9 @@
   kube-controller = callPackage ./kube-controller {};
   kubelet = callPackage ./kubelet {};
   kubectl = callPackage ./kubectl {};
-  #
   # etcd = callPackage ./etcd {};
   # etcdctl = callPackage ./etcd/ctl.nix {};
   # etcdutl = callPackage ./etcd/utl.nix {};
-  #
   # cilium-cli = callPackage ./cilium-cli {};
   # runc = callPackage ./runc {};
 }
