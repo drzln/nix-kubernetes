ae17012f0b94ba8c13c4456de83cf978056e8166
luis <luis@pleme.io>
2025-05-04 16:23:27 -0300

    init

--- pkgs/aws-ebs-csi-driver/default.nix
+++ pkgs/aws-ebs-csi-driver/default.nix
@@ -14,7 +14,7 @@ buildGoModule rec {
     sha256 = "sha256-iT+Plrk0FsDnUISx2ZNSqxx777Yw+xGq8YWWgQpmzGw=";
   };
 
-  vendorHash = "sha256-iT+Plrk0FsDnUISx2ZNSqxx777Yw+xGq8YWWgQpmzGw=";
+  vendorHash = "sha256-FosfFQ471zAMhtnSOXPLtzG6OxnyFigbgpe9DkGjjsY=";
 
   subPackages = ["cmd/ebs-plugin"]; # this is the main binary
 
