c3864ef4d00bc23d1038ed61fc61fde3b9868151
luis <luis@pleme.io>
2025-05-04 20:53:53 -0300

    init

--- pkgs/k9s/default.nix
+++ pkgs/k9s/default.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
     owner = "derailed";
     repo = "k9s";
     rev = version;
-    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+    sha256 = "sha256-0MAnN1ekzHLs25EspDN3xacmDvwXGwKO/5RsCMMwTI8=";
   };
   vendorHash = null;
   subPackages = ["."];
