e29786a23ab6b217135d71063eafdd66b581bd93
luis <luis@pleme.io>
2025-05-04 20:53:11 -0300

    init

--- pkgs/k9s/default.nix
+++ pkgs/k9s/default.nix
@@ -13,26 +13,13 @@ buildGoModule rec {
     rev = version;
     sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
   };
-
   vendorHash = null;
-
   subPackages = ["."];
-
   env.CGO_ENABLED = "0";
-
   ldflags = [
     "-s"
     "-w"
     "-X github.com/derailed/k9s/cmd.version=${version}"
   ];
-
   doCheck = false;
-
-  meta = with lib; {
-    description = "K9s - Kubernetes CLI terminal UI";
-    homepage = "https://k9scli.io";
-    license = licenses.asl20;
-    maintainers = [maintainers.yourGithubHandle];
-    platforms = platforms.linux;
-  };
 }
