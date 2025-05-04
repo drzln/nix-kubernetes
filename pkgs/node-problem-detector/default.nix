402a907c67cef0cbd218545e580d898842c1359e
luis <luis@pleme.io>
2025-05-04 16:04:24 -0300

    init

--- pkgs/node-problem-detector/default.nix
+++ pkgs/node-problem-detector/default.nix
@@ -5,24 +5,19 @@
 }:
 buildGoModule rec {
   pname = "node-problem-detector";
-  version = "0.8.13";
-
+  version = "0.8.20";
   src = fetchFromGitHub {
     owner = "kubernetes";
     repo = "node-problem-detector";
     rev = "v${version}";
     sha256 = "sha256-nNi4YahrO4zwqwR90tIpQCAydGdQbfy5PXCifpP/T7Q=";
   };
-
   vendorHash = null;
-
-  subPackages = ["."]; # root is correct for main.go
-
+  subPackages = ["cmd/nodeproblemdetector"];
   ldflags = [
     "-s"
     "-w"
     "-X main.version=${version}"
   ];
-
   doCheck = false;
 }
