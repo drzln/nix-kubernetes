bd9fb537ab73d7b5491bb06fd54ea1111dbf4c59
luis <luis@pleme.io>
2025-05-04 19:49:15 -0300

    init

--- pkgs/oidc-discovery-provider/default.nix
+++ pkgs/oidc-discovery-provider/default.nix
@@ -10,7 +10,7 @@ buildGoModule rec {
     owner = "spiffe";
     repo = "spire";
     rev = "v${version}";
-    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+    sha256 = "sha256-D6NNG//1rM7EIzawKdMA/8nloqMNAkF75YyFpHvxUkI=";
   };
   vendorHash = "sha256-D6NNG//1rM7EIzawKdMA/8nloqMNAkF75YyFpHvxUkI=";
   subPackages = ["cmd/oidc-discovery-provider"];
