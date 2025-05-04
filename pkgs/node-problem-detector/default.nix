03de6609f0d4b4bfbf1aded9b3242f16703dda37
luis <luis@pleme.io>
2025-05-04 16:02:42 -0300

    init

--- pkgs/node-problem-detector/default.nix
+++ pkgs/node-problem-detector/default.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
     owner = "kubernetes";
     repo = "node-problem-detector";
     rev = "v${version}";
-    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+    sha256 = "sha256-nNi4YahrO4zwqwR90tIpQCAydGdQbfy5PXCifpP/T7Q=";
   };
 
   vendorHash = null;
