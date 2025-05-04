169b305529977b794bffc104efd30a28b4ce2327
luis <luis@pleme.io>
2025-05-04 15:57:10 -0300

    init

--- pkgs/kube-state-metrics/default.nix
+++ pkgs/kube-state-metrics/default.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
     owner = "kubernetes";
     repo = "kube-state-metrics";
     rev = "v${version}";
-    sha256 = "sha256-REPLACE_AFTER_PREFETCH";
+    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
   };
 
   vendorHash = null;
