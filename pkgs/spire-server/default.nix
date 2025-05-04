e31cc2ad9832b465c21a8c41d0fc108c9459dd65
luis <luis@pleme.io>
2025-05-04 19:20:33 -0300

    init

--- pkgs/spire-server/default.nix
+++ pkgs/spire-server/default.nix
@@ -5,30 +5,16 @@
 }:
 buildGoModule rec {
   pname = "spire-server";
-  version = "1.8.7"; # latest stable at time of writing
-
+  version = "1.8.7";
   src = fetchFromGitHub {
     owner = "spiffe";
     repo = "spire";
     rev = "v${version}";
     sha256 = "sha256-D6NNG//1rM7EIzawKdMA/8nloqMNAkF75YyFpHvxUkI=";
   };
-
   vendorHash = "sha256-D6NNG//1rM7EIzawKdMA/8nloqMNAkF75YyFpHvxUkI=";
-
   subPackages = ["cmd/spire-server"];
-
   env.CGO_ENABLED = "0";
-
   ldflags = ["-s" "-w"];
-
   doCheck = false;
-
-  meta = with lib; {
-    description = "SPIRE server for managing SPIFFE identities (mTLS CA)";
-    homepage = "https://spiffe.io";
-    license = licenses.asl20;
-    platforms = platforms.linux;
-    maintainers = [maintainers.yourGithubHandle];
-  };
 }
