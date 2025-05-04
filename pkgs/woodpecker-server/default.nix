42903dcb50e99324e60ac796abaea5d4887a194d
luis <luis@pleme.io>
2025-05-04 20:30:03 -0300

    init

--- pkgs/woodpecker-server/default.nix
+++ pkgs/woodpecker-server/default.nix
@@ -5,34 +5,20 @@
 }:
 buildGoModule rec {
   pname = "woodpecker-server";
-  version = "v2.3.1";
-
+  version = "v3.5.2";
   src = fetchFromGitHub {
     owner = "woodpecker-ci";
     repo = "woodpecker";
     rev = version;
     sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
   };
-
   vendorHash = null;
-
   subPackages = ["cmd/server"];
-
   env.CGO_ENABLED = "0";
-
   ldflags = [
     "-s"
     "-w"
     "-X github.com/woodpecker-ci/woodpecker/version.Version=${version}"
   ];
-
   doCheck = false;
-
-  meta = with lib; {
-    description = "Woodpecker CI server – lightweight CI/CD engine";
-    homepage = "https://woodpecker-ci.org";
-    license = licenses.asl20;
-    maintainers = [maintainers.yourGithubHandle];
-    platforms = platforms.linux;
-  };
 }
