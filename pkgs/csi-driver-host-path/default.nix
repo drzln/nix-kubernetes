09e0193c9cddcee3273638658eb1c5d3149a04ff
luis <luis@pleme.io>
2025-05-04 16:18:49 -0300

    init

--- pkgs/csi-driver-host-path/default.nix
+++ pkgs/csi-driver-host-path/default.nix
@@ -10,7 +10,7 @@ buildGoModule rec {
     owner = "kubernetes-csi";
     repo = "csi-driver-host-path";
     rev = version;
-    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+    sha256 = "sha256-6hyJkPcAQzazobMxGJ/p5OQefvijMVnIyQEmfZWvtMI=";
   };
   vendorHash = null;
   subPackages = ["cmd/hostpathplugin"]; # Entry point is here
