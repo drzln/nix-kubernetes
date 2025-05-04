7d0d697dc515c5cd8c971784f3195cbb5c5a99d1
luis <luis@pleme.io>
2025-05-04 15:28:34 -0300

    init

--- pkgs/coredns/default.nix
+++ pkgs/coredns/default.nix
@@ -1,4 +1,3 @@
-# pkgs/coredns/default.nix
 {
   lib,
   buildGoModule,
@@ -7,18 +6,24 @@
 buildGoModule rec {
   pname = "coredns";
   version = "1.12.1";
-
   src = fetchFromGitHub {
     owner = "coredns";
     repo = "coredns";
     rev = "v${version}";
     sha256 = "sha256-XZoRN907PXNKV2iMn51H/lt8yPxhPupNfJ49Pymdm9Y=";
   };
-  vendorHash = null;
+  vendorHash = "sha256-f7ql30gJNzeldJFM0KXfLReAUSQwdyEmf7xW6lgCkpk=";
   subPackages = ["."];
   ldflags = [
     "-s"
     "-w"
   ];
   doCheck = false;
+  meta = with lib; {
+    description = "CoreDNS is a DNS server that chains plugins and serves as the cluster DNS in Kubernetes.";
+    homepage = "https://coredns.io";
+    license = licenses.asl20;
+    maintainers = [];
+    platforms = platforms.linux;
+  };
 }
