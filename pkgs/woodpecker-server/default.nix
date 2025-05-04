5a8d80b208207bad1484caa6c5ec0dd74ab0913a
luis <luis@pleme.io>
2025-05-04 20:30:22 -0300

    init

--- pkgs/woodpecker-server/default.nix
+++ pkgs/woodpecker-server/default.nix
@@ -10,7 +10,7 @@ buildGoModule rec {
     owner = "woodpecker-ci";
     repo = "woodpecker";
     rev = version;
-    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+    sha256 = "sha256-XV9Cz48mMBLngsz9ss9se7BZ9bOhIxbbOQNFGrRIsOg=";
   };
   vendorHash = null;
   subPackages = ["cmd/server"];
