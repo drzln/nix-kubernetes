13adcdab583fbac53f8cab26ad1f08096c5b15c9
luis <luis@pleme.io>
2025-05-04 04:30:48 -0300

    init

--- pkgs/cilium-health/default.nix
+++ pkgs/cilium-health/default.nix
@@ -19,7 +19,7 @@ in
       repo = "cilium";
       rev = "v${version}";
       # Prefetch once: nix-prefetch-url --type sha256 ... > sha256
-      sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+      sha256 = "sha256-dHdpVXTHLh7UjBXgKMeM0l8Dl555zY8IN65nEtbtycA=";
     };
 
     ############################################################################
