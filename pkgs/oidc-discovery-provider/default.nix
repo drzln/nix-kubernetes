f7a7d8072debb5502fb231704060cebd5eda6e79
luis <luis@pleme.io>
2025-05-04 19:48:47 -0300

    init

--- pkgs/oidc-discovery-provider/default.nix
+++ pkgs/oidc-discovery-provider/default.nix
@@ -12,7 +12,7 @@ buildGoModule rec {
     rev = "v${version}";
     sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
   };
-  vendorHash = null;
+  vendorHash = "sha256-D6NNG//1rM7EIzawKdMA/8nloqMNAkF75YyFpHvxUkI=";
   subPackages = ["cmd/oidc-discovery-provider"];
   env.CGO_ENABLED = "0";
   ldflags = ["-s" "-w"];
