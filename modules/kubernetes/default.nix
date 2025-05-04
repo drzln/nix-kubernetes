8ba44637113b2d4f54dcd86f682c242d832321e9
luis <luis@pleme.io>
2025-05-04 00:54:14 -0300

    init

--- modules/kubernetes/default.nix
+++ modules/kubernetes/default.nix
@@ -1,32 +1,32 @@
 {
-  lib,
-  # config,
+  # lib,
+  config,
   ...
 } @ args: let
-  inherit (lib) mkIf mkMerge;
   cfg = config.blackmatter.components.kubernetes;
-  isMaster = cfg.role == "master" || cfg.role == "single";
-  isWorker = cfg.role == "worker";
+  # inherit (lib) mkIf mkMerge;
+  # isMaster = cfg.role == "master" || cfg.role == "single";
+  # isWorker = cfg.role == "worker";
 in {
-  imports =
-    [
-      # ./options.nix
-      # ./pkgs.nix
-      # ./assertions.nix
-      # ./files.nix
-      # ./firewall.nix
-
-      # ./services/containerd.nix
-      # ./services/kubelet.nix
-      # ./services/kubeadm.nix
-      # ./services/proxy.nix
-    ]
-    ++ lib.optional isWorker [
-    ]
-    ++ lib.optional isMaster [
-      # ./services/etcd.nix
-      # ./services/apiserver.nix
-      # ./services/controller.nix
-      # ./services/scheduler.nix
-    ];
+  # imports =
+  #   [
+  #     # ./options.nix
+  #     # ./pkgs.nix
+  #     # ./assertions.nix
+  #     # ./files.nix
+  #     # ./firewall.nix
+  #
+  #     # ./services/containerd.nix
+  #     # ./services/kubelet.nix
+  #     # ./services/kubeadm.nix
+  #     # ./services/proxy.nix
+  #   ]
+  #   ++ lib.optional isWorker [
+  #   ]
+  #   ++ lib.optional isMaster [
+  #     # ./services/etcd.nix
+  #     # ./services/apiserver.nix
+  #     # ./services/controller.nix
+  #     # ./services/scheduler.nix
+  #   ];
 }
