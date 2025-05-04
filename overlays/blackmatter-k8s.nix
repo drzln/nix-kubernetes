a54390d886af2d4236847f0a477f4423f1578bcd
luis <luis@pleme.io>
2025-05-04 02:33:56 -0300

    init

---
+++ overlays/blackmatter-k8s.nix
@@ -0,0 +1,24 @@
+##############################################################################
+#  overlays/blackmatter-k8s.nix
+#
+#  * Adds a namespace `pkgs.blackmatter.k8s` that contains every package
+#    returned by ./pkgs/default.nix
+#  * Leaves anything else in nixpkgs untouched
+##############################################################################
+self: prev: let
+  # Import the full Kubernetes package set; `callPackage` + `lib`
+  # are taken from *prev* so other overlays can still override them.
+  k8sPkgs = import ../pkgs {
+    inherit (prev) lib callPackage;
+  };
+
+  # Preserve anything an earlier overlay might have put in `blackmatter`
+  existingBlackmatter = prev.blackmatter or {};
+in {
+  # Merge (or create) the `blackmatter` namespace, then attach `k8s`
+  blackmatter =
+    existingBlackmatter
+    // {
+      k8s = k8sPkgs;
+    };
+}
