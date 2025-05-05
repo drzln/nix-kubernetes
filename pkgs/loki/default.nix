a1f8652f4aaa28dd94050422665b8ec90b88a062
luis <luis@pleme.io>
2025-05-04 22:12:13 -0300

    init

--- pkgs/loki/default.nix
+++ pkgs/loki/default.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
     owner = "grafana";
     repo = "loki";
     rev = version;
-    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+    sha256 = "sha256-9EUlznnZczgHXUy784830FvUS6OwaFf7FmUJGeWeXP0=";
   };
 
   vendorHash = null;
