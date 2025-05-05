46e821649e794fb4a3a3728947bf5e8de1e29b4e
luis <luis@pleme.io>
2025-05-04 21:04:01 -0300

    init

--- pkgs/popeye/default.nix
+++ pkgs/popeye/default.nix
@@ -6,33 +6,19 @@
 buildGoModule rec {
   pname = "popeye";
   version = "v0.10.0";
-
   src = fetchFromGitHub {
     owner = "derailed";
     repo = "popeye";
     rev = version;
     sha256 = "sha256-iCsEYbEENDOg69wdWu9QQ8tTGxvaY2i/Hboc6XSYyEM=";
   };
-
   vendorHash = "sha256-aLTzhBMwQHa6twzBC3FyMsZa1vQsBDdg4MpzJWZz3n4=";
-
   subPackages = ["."];
-
   env.CGO_ENABLED = "0";
-
   ldflags = [
     "-s"
     "-w"
     "-X github.com/derailed/popeye/cmd.version=${version}"
   ];
-
   doCheck = false;
-
-  meta = with lib; {
-    description = "Popeye scans your Kubernetes clusters and reports potential issues";
-    homepage = "https://github.com/derailed/popeye";
-    license = licenses.asl20;
-    platforms = platforms.linux;
-    maintainers = [maintainers.yourGithubHandle];
-  };
 }
