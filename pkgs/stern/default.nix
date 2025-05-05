c828f0474b3dc85a5ae250a98e2ac97650c54eb8
luis <luis@pleme.io>
2025-05-04 21:07:25 -0300

    init

--- pkgs/stern/default.nix
+++ pkgs/stern/default.nix
@@ -6,33 +6,19 @@
 buildGoModule rec {
   pname = "stern";
   version = "v1.29.0";
-
   src = fetchFromGitHub {
     owner = "stern";
     repo = "stern";
     rev = version;
     sha256 = "sha256-8Tvhul7GwVbRJqJenbYID8OY5zGzFhIormUwEtLE0Lw=";
   };
-
   vendorHash = "sha256-RLcF7KfKtkwB+nWzaQb8Va9pau+TS2uE9AmJ0aFNsik=";
-
   subPackages = ["."];
-
   env.CGO_ENABLED = "0";
-
   ldflags = [
     "-s"
     "-w"
     "-X github.com/stern/stern/cmd.version=${version}"
   ];
-
   doCheck = false;
-
-  meta = with lib; {
-    description = "Stern - tail multiple Kubernetes pods and containers logs";
-    homepage = "https://github.com/stern/stern";
-    license = licenses.asl20;
-    maintainers = [maintainers.yourGithubHandle];
-    platforms = platforms.linux;
-  };
 }
