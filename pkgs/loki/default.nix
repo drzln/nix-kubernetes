7dfdc5478defc2f8e4f0496e7b6482ce6055df53
luis <luis@pleme.io>
2025-05-05 06:58:46 -0300

    init

--- pkgs/loki/default.nix
+++ pkgs/loki/default.nix
@@ -1,38 +1,24 @@
+# pkgs/loki/default.nix
 {
-  lib,
   buildGoModule,
   fetchFromGitHub,
 }:
 buildGoModule rec {
   pname = "loki";
   version = "v2.9.3";
-
   src = fetchFromGitHub {
     owner = "grafana";
     repo = "loki";
     rev = version;
     sha256 = "sha256-9EUlznnZczgHXUy784830FvUS6OwaFf7FmUJGeWeXP0=";
   };
-
   vendorHash = null;
-
   subPackages = ["cmd/loki"];
-
   env.CGO_ENABLED = "0";
-
   ldflags = [
     "-s"
     "-w"
     "-X github.com/grafana/loki/pkg/util/build.Version=${version}"
   ];
-
   doCheck = false;
-
-  meta = with lib; {
-    description = "Grafana Loki – like Prometheus, but for logs";
-    homepage = "https://grafana.com/oss/loki";
-    license = licenses.asl20;
-    maintainers = [maintainers.yourGithubHandle];
-    platforms = platforms.linux;
-  };
 }
