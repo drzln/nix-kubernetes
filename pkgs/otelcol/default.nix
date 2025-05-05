5f79b72ddbbb54e2be2d324aeec0d4a88d8030c1
luis <luis@pleme.io>
2025-05-04 21:39:38 -0300

    init

--- pkgs/otelcol/default.nix
+++ pkgs/otelcol/default.nix
@@ -12,7 +12,7 @@ buildGoModule rec {
     rev = version;
     sha256 = "sha256-WdMQnAYAdyvS0uyRzvLnhi1HeoWqmUQSIq6MdcP7NfY=";
   };
-  vendorHash = null;
+  vendorHash = "sha256-WdMQnAYAdyvS0uyRzvLnhi1HeoWqmUQSIq6MdcP7NfY=";
   subPackages = ["."];
   env.CGO_ENABLED = "0";
   ldflags = [
