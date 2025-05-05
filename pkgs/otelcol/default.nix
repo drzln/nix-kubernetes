7a1263c687ed4e66a2c9d8ec6a4215f6021ead57
luis <luis@pleme.io>
2025-05-04 21:33:19 -0300

    init

--- pkgs/otelcol/default.nix
+++ pkgs/otelcol/default.nix
@@ -13,7 +13,7 @@ buildGoModule rec {
     sha256 = "sha256-BRZxeTFw4v4LLXPPzIzcjtR/RTckpolGGcB6jyq+ZOA=";
   };
   vendorHash = "sha256-4C5Yz0LDvX0WiAVoPqBfimxc7IXupN8q4XMPwKjlvUA=";
-  subPackages = ["cmd/otelcol"];
+  subPackages = ["cmd/otelcorecol"];
   env.CGO_ENABLED = "0";
   ldflags = [
     "-s"
