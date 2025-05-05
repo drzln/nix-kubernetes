3c5a99a3edaa1b760e38cba68d28563126967eea
luis <luis@pleme.io>
2025-05-04 21:30:16 -0300

    init

--- pkgs/otelcol/default.nix
+++ pkgs/otelcol/default.nix
@@ -10,9 +10,9 @@ buildGoModule rec {
     owner = "open-telemetry";
     repo = "opentelemetry-collector";
     rev = version;
-    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+    sha256 = "sha256-BRZxeTFw4v4LLXPPzIzcjtR/RTckpolGGcB6jyq+ZOA=";
   };
-  vendorHash = null;
+  vendorHash = "sha256-BRZxeTFw4v4LLXPPzIzcjtR/RTckpolGGcB6jyq+ZOA=";
   subPackages = ["cmd/otelcol"];
   env.CGO_ENABLED = "0";
   ldflags = [
