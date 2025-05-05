4c1900b40ba65c1021c627e2126e7a8dc1ce3baf
luis <luis@pleme.io>
2025-05-05 06:38:33 -0300

    init

--- pkgs/csi-snapshot-controller/default.nix
+++ pkgs/csi-snapshot-controller/default.nix
@@ -1,37 +1,23 @@
+# pkgs/csi-snapshot-controller/default.nix
 {
-  lib,
   buildGoModule,
   fetchFromGitHub,
 }:
 buildGoModule rec {
   pname = "csi-snapshot-controller";
-  version = "v6.3.3"; # latest stable at time of writing
-
+  version = "v6.3.3";
   src = fetchFromGitHub {
     owner = "kubernetes-csi";
     repo = "external-snapshotter";
     rev = version;
     sha256 = "sha256-pk9600PSp3smMqOqm8cnB8ITFheiLEWonB5dfht/5Tw=";
   };
-
   vendorHash = null;
-
   subPackages = ["cmd/snapshot-controller"];
-
   env.CGO_ENABLED = "0";
-
   ldflags = [
     "-s"
     "-w"
   ];
-
   doCheck = false;
-
-  meta = with lib; {
-    description = "CSI external snapshot controller for managing PVC snapshots";
-    homepage = "https://github.com/kubernetes-csi/external-snapshotter";
-    license = licenses.asl20;
-    platforms = platforms.linux;
-    maintainers = [maintainers.yourGithubHandle];
-  };
 }
