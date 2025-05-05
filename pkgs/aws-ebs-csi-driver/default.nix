9bcf6c1afd53f2c000e6adf5100f5fc275437e6f
luis <luis@pleme.io>
2025-05-05 03:06:21 -0300

    init

--- pkgs/aws-ebs-csi-driver/default.nix
+++ pkgs/aws-ebs-csi-driver/default.nix
@@ -1,3 +1,4 @@
+# pkgs/aws-ebs-csi-driver/default.nix
 {
   lib,
   buildGoModule,
@@ -6,33 +7,19 @@
 buildGoModule rec {
   pname = "aws-ebs-csi-driver";
   version = "v1.30.0";
-
   src = fetchFromGitHub {
     owner = "kubernetes-sigs";
     repo = "aws-ebs-csi-driver";
     rev = version;
     sha256 = "sha256-iT+Plrk0FsDnUISx2ZNSqxx777Yw+xGq8YWWgQpmzGw=";
   };
-
   vendorHash = "sha256-FosfFQ471zAMhtnSOXPLtzG6OxnyFigbgpe9DkGjjsY=";
-
   subPackages = ["cmd"];
-
   env.CGO_ENABLED = "0";
-
   ldflags = [
     "-s"
     "-w"
     "-X sigs.k8s.io/aws-ebs-csi-driver/pkg/driver.driverVersion=${version}"
   ];
-
   doCheck = false;
-
-  meta = with lib; {
-    description = "CSI driver for AWS EBS volumes";
-    homepage = "https://github.com/kubernetes-sigs/aws-ebs-csi-driver";
-    license = licenses.asl20;
-    maintainers = [maintainers.yourGithubHandle];
-    platforms = platforms.linux;
-  };
 }
