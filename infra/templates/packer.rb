a07ba0b64b9181a3f8fa15132827692233756bb8
luis <luis@pleme.io>
2025-05-05 06:15:56 -0300

    init

--- infra/templates/packer.rb
+++ infra/templates/packer.rb
@@ -1,28 +1,24 @@
 # frozen_string_literal: true
+# infra/templates/packer.rb
 
-# Define an environment to produce Packer AMIs
 template(:packer) do
   product   = :packer
   base_tags = { product: product }
   cidr_base = '10.2'
 
-  # VPC Definition
   resource :aws_vpc, product do
     cidr_block "#{cidr_base}.0.0/16"
     enable_dns_hostnames true
     enable_dns_support true
     tags base_tags.merge({ Name: "#{product}_vpc" })
   end
-
   vpc_id_ref = "${aws_vpc.#{product}.id}"
 
-  # Internet Gateway
   resource :aws_internet_gateway, "#{product}_igw" do
     vpc_id vpc_id_ref
     tags base_tags.merge(Name: "#{product}_igw")
   end
 
-  # Public Subnet
   resource :aws_subnet, "#{product}_public_packer" do
     vpc_id vpc_id_ref
     cidr_block "#{cidr_base}.1.0/24"
@@ -34,7 +30,6 @@ template(:packer) do
   subnet_id_ref = "${aws_subnet.#{product}_public_packer.id}"
   igw_id_ref = "${aws_internet_gateway.#{product}_igw.id}"
 
-  # Route Table
   resource :aws_route_table, "#{product}_public_rt" do
     vpc_id vpc_id_ref
     tags base_tags.merge(Name: "#{product}_public_rt")
@@ -42,14 +37,12 @@ template(:packer) do
 
   rt_id_ref = "${aws_route_table.#{product}_public_rt.id}"
 
-  # Route for Internet Access
   resource :aws_route, "#{product}_default_route" do
     route_table_id rt_id_ref
     destination_cidr_block '0.0.0.0/0'
     gateway_id igw_id_ref
   end
 
-  # Associate Route Table with Public Subnet
   resource :aws_route_table_association, "#{product}_public_assoc" do
     subnet_id subnet_id_ref
     route_table_id rt_id_ref
