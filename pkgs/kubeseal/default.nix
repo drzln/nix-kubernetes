e1e74e4775b3bc64ba9013c8fb2691ccb1867649
luis <luis@pleme.io>
2025-05-04 21:12:33 -0300

    init

---
+++ pkgs/kubeseal/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule rec {
+  pname = "kubeseal";
+  version = "v0.24.6";
+  src = fetchFromGitHub {
+    owner = "bitnami-labs";
+    repo = "sealed-secrets";
+    rev = version;
+    sha256 = "1q2akgwi1r5xpc2gs7k2k96mfkdq2q9n4f4s3p37gbsm3dd5aspn";
+  };
+  vendorHash = null;
+  subPackages = ["cmd/kubeseal"];
+  env.CGO_ENABLED = "0";
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${version}"
+  ];
+  doCheck = false;
+}
