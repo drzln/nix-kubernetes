51ed357d864c66554c97d45ce56064b71b05d6a0
luis <luis@pleme.io>
2025-05-04 21:14:38 -0300

    init

--- pkgs/kubeseal/default.nix
+++ pkgs/kubeseal/default.nix
@@ -12,7 +12,7 @@ buildGoModule rec {
     rev = version;
     sha256 = "sha256-unPqjheT8/2gVQAwvzOvHtG4qTqggf9o0M5iLwl1eh4=";
   };
-  vendorHash = "sha256-unPqjheT8/2gVQAwvzOvHtG4qTqggf9o0M5iLwl1eh4=";
+  vendorHash = "sha256-4BseFdfJjR8Th+NJ82dYsz9Dym1hzDa4kB4bpy71q7Q=";
   subPackages = ["cmd/kubeseal"];
   env.CGO_ENABLED = "0";
   ldflags = [
