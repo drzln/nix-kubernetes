74619c6fd36de1c974f0a437fb4a3f25c61d4e6f
luis <luis@pleme.io>
2025-05-05 06:04:49 -0300

    init

--- infra/bin/config.rb
+++ infra/bin/config.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 # infra/bin/config.rb
 
-AWS_PROFILE ||= 'pleme' # ✅ Define once, prevent redefinition
+AWS_PROFILE ||= 'pleme'
 AWS_REGION ||= 'us-east-1'
 NUKE_CONFIG_PATH ||= 'aws-nuke-config.yml'
 DUMMY_BLOCKLIST_ACCOUNT ||= '000000000000'
