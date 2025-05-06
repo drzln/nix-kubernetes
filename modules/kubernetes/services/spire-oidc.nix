68034fa174b685ecd18a28899a1932aacfa33b72
luis <luis@pleme.io>
2025-05-06 00:24:20 -0300

    init

--- modules/kubernetes/services/spire-oidc.nix
+++ modules/kubernetes/services/spire-oidc.nix
@@ -12,13 +12,11 @@ in {
   options.blackmatter.components.kubernetes.services.spire-oidc = {
     enable = mkEnableOption "Enable SPIRE OIDC Discovery Provider";
   };
-
   config = mkIf cfg.enable {
     systemd.services.spire-oidc = {
       description = "blackmatter.spire-oidc-discovery-provider";
       after = ["spire-server.service"];
       wantedBy = ["multi-user.target"];
-
       serviceConfig = {
         ExecStart = ''
           ${pkg}/bin/oidc-discovery-provider run \
@@ -29,18 +27,14 @@ in {
         User = "spire";
         Group = "spire";
       };
-
       environment = {
         PATH = lib.makeBinPath [pkg pkgs.coreutils];
       };
     };
-
     environment.systemPackages = [pkg];
-
     systemd.tmpfiles.rules = [
       "d /etc/spire 0755 spire spire -"
     ];
-
     environment.etc."spire/oidc.conf".text = ''
       issuer = "https://oidc.example.org"
       audience = ["example-app"]
@@ -48,7 +42,6 @@ in {
       ca_bundle_path = "/etc/spire/ca.crt"
       log_level = "INFO"
     '';
-
     environment.etc."spire/ca.crt".text = "REPLACE_ME";
   };
 }
