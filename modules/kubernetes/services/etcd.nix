f3af5d27e90b08317a185d0b182b259b2e4904f9
luis <luis@pleme.io>
2025-05-05 23:12:57 -0300

    init

--- modules/kubernetes/services/etcd.nix
+++ modules/kubernetes/services/etcd.nix
@@ -1,32 +1,53 @@
 # modules/kubernetes/services/etcd.nix
 {
   lib,
+  pkgs,
   config,
-  require,
   ...
-}: let
-  cfg = config.kubernetes;
-  isCP = cfg.role == "master" || cfg.role == "single";
-  etcdPkg = cfg.etcdPackage or require "etcd";
-in
-  lib.mkIf isCP {
+}:
+with lib; let
+  pkg = pkgs.blackmatter.k8s.etcd;
+  cfg = config.blackmatter.components.kubernetes.services.etcd;
+in {
+  options.blackmatter.components.kubernetes.services.etcd = {
+    enable = mkEnableOption "Enable etcd";
+  };
+  config = mkIf cfg.enable {
+    systemd.services.etcd = {
+      description = "blackmatter.etcd";
+      wantedBy = ["multi-user.target"];
+      after = ["network.target"];
+      serviceConfig = {
+        ExecStart = ''
+          ${pkg}/bin/etcd \
+            --name node1 \
+            --data-dir /var/lib/etcd \
+            --listen-client-urls http://127.0.0.1:2379,http://[::1]:2379 \
+            --advertise-client-urls http://127.0.0.1:2379 \
+            --listen-peer-urls http://127.0.0.1:2380 \
+            --initial-advertise-peer-urls http://127.0.0.1:2380 \
+            --initial-cluster node1=http://127.0.0.1:2380 \
+            --initial-cluster-token etcd-cluster-1 \
+            --initial-cluster-state new
+        '';
+        Restart = "always";
+        RestartSec = 2;
+        LimitNOFILE = 40000;
+      };
+      environment = {
+        PATH = lib.makeBinPath [pkg pkgs.coreutils];
+      };
+    };
+    environment.systemPackages = [
+      pkgs.blackmatter.k8s.etcdctl
+    ];
+    systemd.tmpfiles.rules = [
+      "d /var/lib/etcd 0700 etcd etcd -"
+    ];
     users.users.etcd = {
       isSystemUser = true;
       group = "etcd";
     };
     users.groups.etcd = {};
-
-    systemd.tmpfiles.rules = ["d /var/lib/etcd 0700 etcd etcd"];
-
-    systemd.services.etcd = {
-      description = "Embedded etcd";
-      wantedBy = ["multi-user.target"];
-      serviceConfig = {
-        ExecStart =
-          "${etcdPkg}/bin/etcd --data-dir=/var/lib/etcd "
-          + "--advertise-client-urls=http://127.0.0.1:2379 "
-          + "--listen-client-urls=http://127.0.0.1:2379";
-        Restart = "always";
-      };
-    };
-  }
+  };
+}
