599bc3e6de9f8947c1b572dd2073173d320fd255
luis <luis@pleme.io>
2025-05-06 00:17:12 -0300

    init

--- modules/kubernetes/services/vector.nix
+++ modules/kubernetes/services/vector.nix
@@ -12,19 +12,16 @@ in {
   options.blackmatter.components.kubernetes.services.vector = {
     enable = mkEnableOption "Enable Vector telemetry pipeline";
   };
-
   config = mkIf cfg.enable {
     users.users.vector = {
       isSystemUser = true;
       group = "vector";
     };
     users.groups.vector = {};
-
     systemd.services.vector = {
       description = "blackmatter.vector";
       after = ["network.target"];
       wantedBy = ["multi-user.target"];
-
       serviceConfig = {
         ExecStart = "${pkg}/bin/vector --config /etc/vector/vector.toml";
         Restart = "always";
@@ -34,19 +31,15 @@ in {
         RuntimeDirectory = "vector";
         LimitNOFILE = 65536;
       };
-
       environment = {
         PATH = lib.makeBinPath [pkg pkgs.coreutils];
       };
     };
-
     environment.systemPackages = [pkg];
-
     systemd.tmpfiles.rules = [
       "d /etc/vector 0755 vector vector -"
       "d /var/lib/vector 0755 vector vector -"
     ];
-
     environment.etc."vector/vector.toml".text = ''
       [sources.kubernetes_logs]
       type = "kubernetes_logs"
