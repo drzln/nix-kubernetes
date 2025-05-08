468967c4e12ab305ab97940b1f1fd722caae7621
luis <luis@pleme.io>
2025-05-07 21:03:20 -0300

    init

--- pkgs/runc/default.nix
+++ pkgs/runc/default.nix
@@ -13,8 +13,8 @@ in
       owner = "opencontainers";
       repo = "runc";
       rev = "v${version}";
-      # sha256 = "sha256-nTVjgNMnB6775ubzK7ezOxR5Z0z5PBxx88CxtbxGxrY=";
-      hash = "sha256-oXoDio3l23Z6UyAhb9oDMo1O4TLBbFyLh9sRWXnfLVY=";
+      sha256 = "sha256-nTVjgNMnB6775ubzK7ezOxR5Z0z5PBxx88CxtbxGxrY=";
+      # hash = "sha256-oXoDio3l23Z6UyAhb9oDMo1O4TLBbFyLh9sRWXnfLVY=";
     };
     vendorHash = null;
     subPackages = ["."];
