a0677c5e44adc5a46d883bea98b17d2539ddbb98
luis <luis@pleme.io>
2025-05-07 21:53:52 -0300

    init

--- pkgs/runc/default.nix
+++ pkgs/runc/default.nix
@@ -10,17 +10,11 @@ buildGoModule rec {
     owner = "opencontainers";
     repo = "runc";
     rev = "v${version}";
-    sha256 = "sha256-nTVjgNMnB6775ubzK7ezOxR5Z0z5PBxx88CxtbxGxrY=";
+    hash = "sha256-nTVjgNMnB6775ubzK7ezOxR5Z0z5PBxx88CxtbxGxrY=";
   };
-  vendorHash = null;
+  vendorHash = "sha256-QK9F2tp8l7+KT9B2wyu1W9n6bt5ioei5dz8mD5vx9q0=";
   subPackages = ["."];
-  env.CGO_ENABLED = 0;
-  buildFlags = ''
-    -tags ""
-    -ldflags "-s -w -X main.version=${version}"
-  '';
-  # installPhase = ''
-  #   install -D -m755 $GOBIN/runc $out/bin/runc
-  # '';
-  doCheck = false;
+  CGO_ENABLED = "0"; # static binary, no cgo
+  ldflags = ["-s" "-w" "-X" "main.version=${version}"];
+  doCheck = false; # upstream tests need root / network
 }
