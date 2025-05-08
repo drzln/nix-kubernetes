a88be6e90ee80da250d8cf45dc8838f438ae019e
luis <luis@pleme.io>
2025-05-07 21:31:40 -0300

    init

--- pkgs/runc/default.nix
+++ pkgs/runc/default.nix
@@ -2,28 +2,25 @@
 {
   buildGoModule,
   fetchFromGitHub,
-  ...
-}: let
+}:
+buildGoModule rec {
+  pname = "runc";
   version = "1.3.0";
-in
-  buildGoModule {
-    pname = "runc";
-    inherit version;
-    src = fetchFromGitHub {
-      owner = "opencontainers";
-      repo = "runc";
-      rev = "v${version}";
-      sha256 = "sha256-nTVjgNMnB6775ubzK7ezOxR5Z0z5PBxx88CxtbxGxrY=";
-      # hash = "sha256-oXoDio3l23Z6UyAhb9oDMo1O4TLBbFyLh9sRWXnfLVY=";
-    };
-    vendorHash = null;
-    subPackages = ["."];
-    env.CGO_ENABLED = "0";
-    ldflags = [
-      "-s"
-      "-w"
-      "-X main.gitCommit=${version}"
-      "-X main.version=${version}"
-    ];
-    doCheck = false;
-  }
+  src = fetchFromGitHub {
+    owner = "opencontainers";
+    repo = "runc";
+    rev = "v${version}";
+    sha256 = "sha256-nTVjgNMnB6775ubzK7ezOxR5Z0z5PBxx88CxtbxGxrY=";
+  };
+  vendorHash = null;
+  subPackages = ["."];
+  CGO_ENABLED = 0;
+  buildFlags = ''
+    -tags ""
+    -ldflags "-s -w -X main.version=${version}"
+  '';
+  installPhase = ''
+    install -D -m755 $GOBIN/runc $out/bin/runc
+  '';
+  doCheck = false;
+}
