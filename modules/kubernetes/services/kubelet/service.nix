f33df4efe853d284c31204cda09b82dd159c9877
luis <luis@pleme.io>
2025-05-10 09:12:37 -0300

    init

--- modules/kubernetes/services/kubelet/service.nix
+++ modules/kubernetes/services/kubelet/service.nix
@@ -1,36 +1,44 @@
 # modules/kubernetes/services/kubelet/service.nix
 {
-  lib,
-  pkgs,
-  cfg,
   blackmatterPkgs,
+  pkgs,
+  lib,
+  cfg,
 }: let
   scr = "/run/secrets/kubernetes";
   pkg = blackmatterPkgs.kubelet;
 in {
   environment.systemPackages = [pkg];
-
   systemd.tmpfiles.rules = [
     "d /etc/kubernetes/manifests 0755 root root -"
     "d /etc/kubernetes/kubelet   0755 root root -"
     "d /etc/cni/net.d            0755 root root -"
     "d /var/run/etcd             0700 root root -"
   ];
-
   systemd.services.kubelet = {
     description = "blackmatter.kubelet";
-    after = ["network.target" "containerd.service" "systemd-tmpfiles-setup.service"];
     wantedBy = ["multi-user.target"];
+    after = ["network.target" "containerd.service" "systemd-tmpfiles-setup.service"];
     environment.PATH = lib.mkForce (lib.makeBinPath [
-      pkg
-      pkgs.runc
-      pkgs.iproute2
-      pkgs.coreutils
-      pkgs.util-linux
-      pkgs.containerd
       blackmatterPkgs.cilium-cni
+      pkgs.containerd
+      pkgs.util-linux
+      pkgs.coreutils
+      pkgs.iproute2
+      pkgs.runc
+      pkg
     ]);
     serviceConfig = {
+      CapabilityBoundingSet = ["CAP_SYSLOG" "CAP_SYS_ADMIN"];
+      AmbientCapabilities = ["CAP_SYSLOG" "CAP_SYS_ADMIN"];
+      DeviceAllow = ["/dev/kmsg r"];
+      ProtectKernelLogs = false;
+      PrivateDevices = false;
+      LimitNOFILE = 1048576;
+      KillMode = "process";
+      Restart = "always";
+      Delegate = true;
+      RestartSec = 2;
       User = "root";
       ExecStart = lib.concatStringsSep " " (
         [
@@ -40,16 +48,6 @@ in {
         ]
         ++ cfg.extraFlags
       );
-      Restart = "always";
-      RestartSec = 2;
-      KillMode = "process";
-      Delegate = true;
-      LimitNOFILE = 1048576;
-      CapabilityBoundingSet = ["CAP_SYSLOG" "CAP_SYS_ADMIN"];
-      AmbientCapabilities = ["CAP_SYSLOG" "CAP_SYS_ADMIN"];
-      DeviceAllow = ["/dev/kmsg r"];
-      PrivateDevices = false;
-      ProtectKernelLogs = false;
     };
   };
 }
