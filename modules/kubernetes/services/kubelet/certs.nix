baf2286a0945f4fdd7697c45f59866724cbd97c3
drzln <drzzln@protonmail.com>
2025-05-14 09:26:42 -0300

    init

--- modules/kubernetes/services/kubelet/certs.nix
+++ modules/kubernetes/services/kubelet/certs.nix
@@ -3,25 +3,26 @@
   kubeletCertGen = pkgs.stdenv.mkDerivation {
     pname = "kubelet-cert-gen";
     version = "1.0";
+
     src = ./generate-certs.sh;
     dontUnpack = true;
-    nativeBuildInputs = [pkgs.makeWrapper];
-    buildInputs = with pkgs;[ iproute2 gawk ];
+
+    nativeBuildInputs = with pkgs; [makeWrapper];
+
     installPhase = ''
       mkdir -p $out/bin
       cp $src $out/bin/generate-certs.sh
       chmod +x $out/bin/generate-certs.sh
       patchShebangs $out/bin
-    '';
-    postInstall = ''
+
       wrapProgram $out/bin/generate-certs.sh \
         --prefix PATH : ${pkgs.lib.makeBinPath [
         pkgs.bash
-        pkgs.openssl
-        pkgs.iproute2
         pkgs.coreutils
+        pkgs.iproute2
         pkgs.gawk
         pkgs.gnugrep
+        pkgs.openssl
       ]}
     '';
   };
