f277ba0580e58263e5f32dca96d550c793e1c887
drzln <drzzln@protonmail.com>
2025-05-14 07:14:50 -0300

    init

--- modules/kubernetes/services/kubelet/assets.nix
+++ modules/kubernetes/services/kubelet/assets.nix
@@ -1,10 +1,7 @@
 # modules/kubernetes/services/kubelet/assets.nix
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    openssl
-  ];
   environment.etc."kubernetes/scripts/generate-certs.sh".text = builtins.readFile ./generate-certs.sh;
-  environment.etc."kubernetes/scripts/verify-certs.sh".text = builtins.readFile ./verify-certs.sh;
+  # environment.etc."kubernetes/scripts/verify-certs.sh".text = builtins.readFile ./verify-certs.sh;
   systemd.services.kubelet-generate-certs = {
     description = "Generate TLS certs and configs for kubelet";
     wantedBy = ["multi-user.target"];
@@ -13,14 +10,14 @@
       ExecStart = "${pkgs.bash}/bin/bash /etc/kubernetes/scripts/generate-certs.sh";
     };
   };
-  systemd.services.kubelet-verify-certs = {
-    description = "Verify TLS certs for kubelet";
-    wantedBy = ["multi-user.target"];
-    before = ["kubelet.service"];
-    after = ["kubelet-generate-certs.service"];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.bash}/bin/bash /etc/kubernetes/scripts/verify-certs.sh";
-    };
-  };
+  # systemd.services.kubelet-verify-certs = {
+  #   description = "Verify TLS certs for kubelet";
+  #   wantedBy = ["multi-user.target"];
+  #   before = ["kubelet.service"];
+  #   after = ["kubelet-generate-certs.service"];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = "${pkgs.bash}/bin/bash /etc/kubernetes/scripts/verify-certs.sh";
+  #   };
+  # };
 }
