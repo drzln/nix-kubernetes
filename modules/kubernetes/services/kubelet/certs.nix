827e6a9d853576841feda6122f61e2022e3d2be7
drzln <drzzln@protonmail.com>
2025-05-14 10:01:59 -0300

    init

--- modules/kubernetes/services/kubelet/certs.nix
+++ modules/kubernetes/services/kubelet/certs.nix
@@ -5,7 +5,7 @@
   lib,
   ...
 }: let
-  cfg = config.blackmatter.components.kubernetes.services.kubelet.certs;
+  cfg = config.blackmatter.components.kubernetes.services.kubelet;
   kubeletCertGen = pkgs.stdenv.mkDerivation {
     pname = "kubelet-cert-gen";
     version = "1.0";
