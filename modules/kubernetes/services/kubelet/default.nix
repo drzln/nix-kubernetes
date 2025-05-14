e7c4cc000b3ea78c5e8f57cb0b22737d33697212
drzln <drzzln@protonmail.com>
2025-05-14 08:33:13 -0300

    init

--- modules/kubernetes/services/kubelet/default.nix
+++ modules/kubernetes/services/kubelet/default.nix
@@ -1,42 +1,15 @@
 # modules/kubernetes/services/kubelet/default.nix
 {
   lib,
-  pkgs,
   config,
-  blackmatterPkgs,
   ...
 }: let
   cfg = config.blackmatter.components.kubernetes.services.kubelet;
 in {
   imports = [
     ./options.nix
+    ./service.nix
+    ./assets.nix
+    (lib.mkIf cfg.staticControlPlane.enable ./static-pods.nix)
   ];
-
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    (import ./service.nix {
-      inherit lib pkgs cfg blackmatterPkgs;
-    })
-
-    (import ./assets.nix {
-      inherit lib pkgs;
-      cfg = cfg;
-    })
-
-    (lib.mkIf cfg.staticControlPlane.enable (
-      lib.mkMerge (
-        (import ./static-pods.nix {inherit lib cfg;})
-        ++ [
-          {
-            networking.firewall.allowedTCPPorts = [
-              10257
-              10259
-              6443
-              2379
-              2380
-            ];
-          }
-        ]
-      )
-    ))
-  ]);
 }
