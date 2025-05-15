9932fcba15f0204d4a949ab5ba32133f604c9488
drzln <drzzln@protonmail.com>
2025-05-15 11:10:57 -0300

    init

--- modules/kubernetes/services/kubelet/static-pods/etcd/default.nix
+++ modules/kubernetes/services/kubelet/static-pods/etcd/default.nix
@@ -1,3 +1,4 @@
+# modules/kubernetes/services/kubelet/static-pods/etcd/default.nix
 {
   pkgs,
   lib,
@@ -6,7 +7,12 @@
   pki = "/var/lib/blackmatter/certs";
   podLib = import ../pod-lib.nix {inherit pkgs lib;};
   image = "quay.io/coreos/etcd:v3.5.9";
-  manifest =
-    podLib.manifestFile "etcd.json"
-    (podLib.mkEtcdPod pki image);
-in {inherit manifest;}
+in {
+  options.manifest = lib.mkOption {
+    type = lib.types.package;
+    description = "The generated static pod manifest for etcd";
+  };
+
+  config.manifest =
+    podLib.manifestFile "etcd.json" (podLib.mkEtcdPod pki image);
+}
