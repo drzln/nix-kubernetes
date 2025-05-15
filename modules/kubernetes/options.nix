d5ddf38560780c555fe1ff432703eab577c44ef6
drzln <drzzln@protonmail.com>
2025-05-15 04:38:55 -0300

    init

--- modules/kubernetes/options.nix
+++ modules/kubernetes/options.nix
@@ -3,65 +3,9 @@
 with lib; {
   options.kubernetes = {
     enable = mkEnableOption "kubernetes";
-
     role = mkOption {
-      type = types.enum ["master" "worker" "single"];
-      default = "master";
-    };
-
-    overlay = mkOption {
-      type = types.nullOr types.anything;
-      default = null;
-    };
-
-    etcdPackage = mkOption {
-      type = types.nullOr types.package;
-      default = null;
-    };
-
-    containerdPackage = mkOption {
-      type = types.nullOr types.package;
-      default = null;
-    };
-
-    nodePortRange = mkOption {
-      type = types.nullOr types.str;
-      default = null;
-    };
-
-    extraApiArgs = mkOption {
-      type = types.listOf types.str;
-      default = [];
-    };
-
-    extraKubeletOpts = mkOption {
-      type = types.listOf types.str;
-      default = [];
-    };
-
-    kubeadmExtra = mkOption {
-      type = types.str;
-      default = "";
-    };
-
-    firewallOpen = mkOption {
-      type = types.bool;
-      default = false;
-    };
-
-    join.address = mkOption {
-      type = types.str;
-      default = "";
-    };
-
-    join.token = mkOption {
-      type = types.str;
-      default = "";
-    };
-
-    join.caHash = mkOption {
-      type = types.str;
-      default = "";
+      type = types.enum ["single" "master" "worker"];
+      default = "single";
     };
   };
 }
