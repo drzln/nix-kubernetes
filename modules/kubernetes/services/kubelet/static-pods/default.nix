bfe03fbdbdea60b3b72b2846d94ba1ba36677ade
drzln <drzzln@protonmail.com>
2025-05-15 15:53:12 -0300

    init

--- modules/kubernetes/services/kubelet/static-pods/default.nix
+++ modules/kubernetes/services/kubelet/static-pods/default.nix
@@ -6,7 +6,9 @@
   ...
 }: let
   cfg = config.blackmatter.components.kubernetes.kubelet.static-pods;
+
   script = pkgs.writeShellScriptBin "setup-manifests" (builtins.readFile ./setup-manifests.sh);
+
   podManifests = lib.flatten [
     (import ./etcd {inherit config pkgs lib;}).config.manifest
     (import ./kube-apiserver {inherit config pkgs lib;}).config.manifest
@@ -20,28 +22,37 @@ in {
     ./kube-scheduler
     ./kube-controller-manager
   ];
+
   options.blackmatter.components.kubernetes.kubelet.static-pods.enable =
     lib.mkEnableOption "Enable static pods for Kubernetes components";
+
   config = lib.mkIf cfg.enable {
     system.activationScripts.restart-static-pods = ''
       echo "[+] Restarting static-pods service..."
       ${pkgs.systemd}/bin/systemctl restart static-pods.service
     '';
+
     systemd.services.static-pods = {
       description = "Setup static pod manifests";
+
+      # Fix applied here: Move Wants and After to [Unit] section.
+      wants = [
+        "kubelet-generate-certs.service"
+        "network.target"
+        "containerd.service"
+      ];
+
+      after = [
+        "network.target"
+        "containerd.service"
+        "systemd-tmpfiles-setup.service"
+        "kubelet-generate-certs.service"
+      ];
+
       before = ["kubelet.service"];
       wantedBy = ["multi-user.target"];
+
       serviceConfig = {
-        Wants = [
-          "kubelet-generate-certs.service"
-          "static-pods.service"
-        ];
-        After = [
-          "network.target"
-          "containerd.service"
-          "systemd-tmpfiles-setup.service"
-          "kubelet-generate-certs.service"
-        ];
         Type = "oneshot";
         RemainAfterExit = true;
         ExecStart = ''
