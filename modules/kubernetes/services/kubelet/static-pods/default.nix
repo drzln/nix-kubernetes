b81dbf3a3fd15d24b2d0c999255d0df1bcb6ee03
drzln <drzzln@protonmail.com>
2025-05-15 09:05:37 -0300

    init

--- modules/kubernetes/services/kubelet/static-pods/default.nix
+++ modules/kubernetes/services/kubelet/static-pods/default.nix
@@ -10,15 +10,15 @@
   podManifests = lib.flatten [
     (import ./etcd {inherit config pkgs lib;}).manifest
     (import ./kube-apiserver {inherit config pkgs lib;}).manifest
-    (import ./kube-controller-manager {inherit config pkgs lib;}).manifest
     (import ./kube-scheduler {inherit config pkgs lib;}).manifest
+    (import ./kube-controller-manager {inherit config pkgs lib;}).manifest
   ];
 in {
   imports = [
     ./etcd
     ./kube-apiserver
-    ./kube-controller-manager
     ./kube-scheduler
+    ./kube-controller-manager
   ];
   options.blackmatter.components.kubernetes.static-pods.enable =
     lib.mkEnableOption "Enable static pods for Kubernetes components";
