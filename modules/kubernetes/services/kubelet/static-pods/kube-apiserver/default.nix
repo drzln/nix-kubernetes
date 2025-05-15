891ed4b1d1e26248e31334442876da0b7f657880
drzln <drzzln@protonmail.com>
2025-05-15 11:28:46 -0300

    init

--- modules/kubernetes/services/kubelet/static-pods/kube-apiserver/default.nix
+++ modules/kubernetes/services/kubelet/static-pods/kube-apiserver/default.nix
@@ -1,3 +1,4 @@
+# modules/kubernetes/services/kubelet/static-pods/kube-apiserver/default.nix
 {
   config,
   pkgs,
@@ -6,10 +7,15 @@
 }: let
   cfg = config.blackmatter.components.kubernetes.static-pods;
   pki = "/var/lib/blackmatter/certs";
+  svcCIDR = cfg.serviceCIDR;
   podLib = import ../pod-lib.nix {inherit pkgs lib;};
   image = "registry.k8s.io/kube-apiserver:${cfg.kubernetesVersion}";
-  svcCIDR = cfg.serviceCIDR;
-  manifest =
-    podLib.manifestFile "kube-apiserver.json"
-    (podLib.mkApiServerPod pki svcCIDR image);
-in {inherit manifest;}
+in {
+  options.manifest = lib.mkOption {
+    type = lib.types.package;
+    description = "The generated static pod manifest for kube-apiserver";
+  };
+
+  config.manifest =
+    podLib.manifestFile "kube-apiserver.json" (podLib.mkApiServerPod pki svcCIDR image);
+}
