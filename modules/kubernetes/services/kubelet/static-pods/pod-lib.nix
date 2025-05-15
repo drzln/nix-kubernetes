2177f468a069324a2ad6703ab219f9d58d2fef51
drzln <drzzln@protonmail.com>
2025-05-15 07:16:46 -0300

    init

--- modules/kubernetes/services/kubelet/static-pods/pod-lib.nix
+++ modules/kubernetes/services/kubelet/static-pods/pod-lib.nix
@@ -51,69 +51,69 @@
     ]
     image {};
 
-  mkApiServerPod = pki: svcCIDR: image:
-    mkPod pki "kube-apiserver" [
-      "kube-apiserver"
-      "--advertise-address=127.0.0.1"
-      "--secure-port=6443"
-      "--etcd-servers=https://127.0.0.1:2379"
-      "--client-ca-file=${pki}/ca.crt"
-      "--tls-cert-file=${pki}/apiserver.crt"
-      "--tls-private-key-file=${pki}/apiserver.key"
-      "--service-cluster-ip-range=${svcCIDR}"
-      "--authorization-mode=Node,RBAC"
-    ]
-    image {};
+  # mkApiServerPod = pki: svcCIDR: image:
+  #   mkPod pki "kube-apiserver" [
+  #     "kube-apiserver"
+  #     "--advertise-address=127.0.0.1"
+  #     "--secure-port=6443"
+  #     "--etcd-servers=https://127.0.0.1:2379"
+  #     "--client-ca-file=${pki}/ca.crt"
+  #     "--tls-cert-file=${pki}/apiserver.crt"
+  #     "--tls-private-key-file=${pki}/apiserver.key"
+  #     "--service-cluster-ip-range=${svcCIDR}"
+  #     "--authorization-mode=Node,RBAC"
+  #   ]
+  #   image {};
 
-  mkControllerManagerPod = pki: scr: image:
-    mkPod pki "kube-controller-manager" [
-      "kube-controller-manager"
-      "--kubeconfig=${scr}/configs/controller-manager/kubeconfig"
-      "--cluster-signing-cert-file=${pki}/ca.crt"
-      "--cluster-signing-key-file=${pki}/ca.key"
-      "--root-ca-file=${pki}/ca.crt"
-      "--service-account-private-key-file=${pki}/ca.key"
-    ]
-    image {
-      volumes = [
-        {
-          name = "kubeconfig";
-          hostPath = {
-            path = "${scr}/configs/controller-manager/kubeconfig";
-            type = "File";
-          };
-        }
-      ];
-      volumeMounts = [
-        {
-          name = "kubeconfig";
-          mountPath = "${scr}/configs/controller-manager/kubeconfig";
-          readOnly = true;
-        }
-      ];
-    };
+  # mkControllerManagerPod = pki: scr: image:
+  #   mkPod pki "kube-controller-manager" [
+  #     "kube-controller-manager"
+  #     "--kubeconfig=${scr}/configs/controller-manager/kubeconfig"
+  #     "--cluster-signing-cert-file=${pki}/ca.crt"
+  #     "--cluster-signing-key-file=${pki}/ca.key"
+  #     "--root-ca-file=${pki}/ca.crt"
+  #     "--service-account-private-key-file=${pki}/ca.key"
+  #   ]
+  #   image {
+  #     volumes = [
+  #       {
+  #         name = "kubeconfig";
+  #         hostPath = {
+  #           path = "${scr}/configs/controller-manager/kubeconfig";
+  #           type = "File";
+  #         };
+  #       }
+  #     ];
+  #     volumeMounts = [
+  #       {
+  #         name = "kubeconfig";
+  #         mountPath = "${scr}/configs/controller-manager/kubeconfig";
+  #         readOnly = true;
+  #       }
+  #     ];
+  #   };
 
-  mkSchedulerPod = scr: image:
-    mkPod "/dev/null" "kube-scheduler" [
-      "kube-scheduler"
-      "--kubeconfig=${scr}/configs/scheduler/kubeconfig"
-    ]
-    image {
-      volumes = [
-        {
-          name = "kubeconfig";
-          hostPath = {
-            path = "${scr}/configs/scheduler/kubeconfig";
-            type = "File";
-          };
-        }
-      ];
-      volumeMounts = [
-        {
-          name = "kubeconfig";
-          mountPath = "${scr}/configs/scheduler/kubeconfig";
-          readOnly = true;
-        }
-      ];
-    };
+  # mkSchedulerPod = scr: image:
+  #   mkPod "/dev/null" "kube-scheduler" [
+  #     "kube-scheduler"
+  #     "--kubeconfig=${scr}/configs/scheduler/kubeconfig"
+  #   ]
+  #   image {
+  #     volumes = [
+  #       {
+  #         name = "kubeconfig";
+  #         hostPath = {
+  #           path = "${scr}/configs/scheduler/kubeconfig";
+  #           type = "File";
+  #         };
+  #       }
+  #     ];
+  #     volumeMounts = [
+  #       {
+  #         name = "kubeconfig";
+  #         mountPath = "${scr}/configs/scheduler/kubeconfig";
+  #         readOnly = true;
+  #       }
+  #     ];
+  #   };
 }
