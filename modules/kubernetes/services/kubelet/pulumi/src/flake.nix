defa727bc14d1cb7d5838d879c9ddae81b18bdd8
drzln <drzzln@protonmail.com>
2025-05-16 05:55:17 -0300

    init

---
+++ modules/kubernetes/services/kubelet/pulumi/src/flake.nix
@@ -0,0 +1,57 @@
+# flake.nix
+{
+  description = "Flake for Golang-based Pulumi Kubernetes provisioning";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    # self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        pulumi-go-env = pkgs.buildGoModule {
+          pname = "kubelet-provisioning";
+          version = "0.1.0";
+
+          src = ./.;
+
+          vendorHash = null;
+
+          nativeBuildInputs = [pkgs.pulumi pkgs.git];
+
+          buildPhase = ''
+            go build -o $out/bin/pulumi-kubelet
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp pulumi-kubelet $out/bin/
+          '';
+        };
+      in {
+        packages.default = pulumi-go-env;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.gopls
+            pkgs.gotools
+            pkgs.pulumi
+            pkgs.pulumiPackages.pulumi-kubernetes
+            pkgs.kubectl
+          ];
+
+          shellHook = ''
+            export KUBECONFIG=/run/secrets/kubernetes/configs/admin/kubeconfig
+          '';
+        };
+      }
+    );
+}
