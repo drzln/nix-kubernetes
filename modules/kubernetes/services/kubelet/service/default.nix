563540e171955aa7f5d0f547b8d23a14183bb801
drzln <drzzln@protonmail.com>
2025-05-16 07:26:57 -0300

    init

--- modules/kubernetes/services/kubelet/service/default.nix
+++ modules/kubernetes/services/kubelet/service/default.nix
@@ -1,77 +1,9 @@
-# modules/kubernetes/services/kubelet/service.nix
-{
-  blackmatterPkgs,
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  cfg = config.blackmatter.components.kubernetes.kubelet.service;
-  scr = "/run/secrets/kubernetes";
-  pkg = blackmatterPkgs.blackmatter.k8s.kubelet;
-in {
-  options.blackmatter.components.kubernetes.kubelet.service = {
-    enable = lib.mkEnableOption "Enable the kubelet systemd service unit";
-  };
-
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [pkg];
-
-    systemd.tmpfiles.rules = [
-      "d /etc/kubernetes/manifests 0755 root root -"
-      "d /etc/kubernetes/kubelet   0755 root root -"
-      "d /etc/cni/net.d            0755 root root -"
-      "d /var/run/etcd             0700 root root -"
-    ];
-
-    system.activationScripts.restart-kubelet = ''
-      echo "[+] Restarting kubelet service..."
-      ${pkgs.systemd}/bin/systemctl restart kubelet.service
-    '';
-
-    systemd.services.kubelet = {
-      description = "blackmatter.kubelet";
-      wantedBy = ["multi-user.target"];
-
-      environment.PATH = lib.mkForce (lib.makeBinPath [
-        blackmatterPkgs.blackmatter.k8s.cilium-cni
-        pkgs.containerd
-        pkgs.util-linux
-        pkgs.coreutils
-        pkgs.iproute2
-        pkgs.runc
-        pkg
-      ]);
-
-      serviceConfig = {
-        Wants = [
-          "kubelet-generate-certs.service"
-          "static-pods.service"
-        ];
-        After = [
-          "network.target"
-          "containerd.service"
-          "systemd-tmpfiles-setup.service"
-          "kubelet-generate-certs.service"
-          "static-pods.service"
-        ];
-        CapabilityBoundingSet = ["CAP_SYSLOG" "CAP_SYS_ADMIN"];
-        AmbientCapabilities = ["CAP_SYSLOG" "CAP_SYS_ADMIN"];
-        DeviceAllow = ["/dev/kmsg r"];
-        ProtectKernelLogs = false;
-        PrivateDevices = false;
-        LimitNOFILE = 1048576;
-        KillMode = "process";
-        Restart = "always";
-        Delegate = true;
-        RestartSec = 2;
-        User = "root";
-        ExecStart = lib.concatStringsSep " " [
-          "${pkg}/bin/kubelet"
-          "--config=${scr}/configs/kubelet/config"
-          "--kubeconfig=${scr}/configs/kubelet/kubeconfig"
-        ];
-      };
-    };
-  };
+# modules/kubernetes/services/kubelet/service/default.nix
+{...}: {
+  imports = [
+    ./config.nix
+    ./tmpfiles.nix
+    ./service.nix
+    ./activation.nix
+  ];
 }
