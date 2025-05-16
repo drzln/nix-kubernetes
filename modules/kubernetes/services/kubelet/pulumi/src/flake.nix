a9823139f76300e7c3b237e89671ebd217e06a25
drzln <drzzln@protonmail.com>
2025-05-16 05:59:41 -0300

    init

--- modules/kubernetes/services/kubelet/pulumi/src/flake.nix
+++ modules/kubernetes/services/kubelet/pulumi/src/flake.nix
@@ -1,12 +1,10 @@
 # flake.nix
 {
-  description = "Flake for Golang-based Pulumi Kubernetes provisioning";
-
+  description = "golang pulumi";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = {
     nixpkgs,
     flake-utils,
@@ -15,21 +13,15 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-
         pulumi-go-env = pkgs.buildGoModule {
           pname = "kubelet-provisioning";
           version = "0.1.0";
-
           src = ./.;
-
           vendorHash = null;
-
           nativeBuildInputs = [pkgs.pulumi pkgs.git];
-
           buildPhase = ''
             go build -o $out/bin/pulumi-kubelet
           '';
-
           installPhase = ''
             mkdir -p $out/bin
             cp pulumi-kubelet $out/bin/
@@ -37,17 +29,16 @@
         };
       in {
         packages.default = pulumi-go-env;
-
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.go
             pkgs.gopls
             pkgs.gotools
             pkgs.pulumi
-            pkgs.pulumiPackages.pulumi-kubernetes
+            # pkgs.pulumiPackages.pulumi-kubernetes
             pkgs.kubectl
+            pulumi-go-env
           ];
-
           shellHook = ''
             export KUBECONFIG=/run/secrets/kubernetes/configs/admin/kubeconfig
           '';
